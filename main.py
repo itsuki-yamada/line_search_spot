9a7b1d2c7dd902b0b622e223247b365ad55458ae
yamadaitsuki <yamada351035@gmail.com>
2019-11-24 03:56:25 +0900

    ref:不要なcommentを消しました。

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ def index():
     return 'You call index()'
 
 
-from func.search_spot import search_local_spot
 
 
 @app.route('/push_sample')
 def push_sample():
+    from func.search_spot import search_local_spot
     """プッシュメッセージを送る"""
     user_id = os.environ['USER_ID']
     line_bot_api.push_message(user_id, TextSendMessage(text='Hello World!'))
@@ -53,9 +53,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    from func.main import callback_area_code, callback_local_spot
-    area_code = callback_area_code(event.message.text)
-    spot = callback_local_spot(area_code)
+    from func.search_spot import search_area_code,search_local_spot
+    area_code = search_area_code(event.message.text)
+    spot = random.choice([town for town in search_local_spot(area_code)])
 
     line_bot_api.reply_message(event.reply_token,
                                TextSendMessage(text=f'{event.message.text}'
@@ -73,10 +73,6 @@ def handle_message(event):
     location_dict = ast.literal_eval(str(event.message))
     spots = search_local_spot(kwargs={'lat': location_dict['latitude'], 'lon': location_dict["longitude"]})
     spot = random.choice([sp for sp in spots])
-    # line_bot_api.reply_message(event.reply_token,
-    #                            TextSendMessage(text=f'{spot.name}'
-    #                                                 f'{spots}'
-    #                                            ))
 
     line_bot_api.reply_message(event.reply_token,
                                LocationSendMessage(type="location",
